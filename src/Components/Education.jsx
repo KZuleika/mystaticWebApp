e97111ad7f773b6f25b4940f0c4ae495a858b299
Karime Zuleika <67844882+KZuleika@users.noreply.github.com>
2023-09-28 04:37:18 +0000

    education

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,66 +1,44 @@
 import React from "react";
-import image from "../images/design-desk.jpeg";
-import imageAltText from "../images/design-desk.jpeg";
-import projectList from "../images/design-desk.jpeg";
+
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description =
+  "Mechatronics Engineering";
+
+const universityname = 
+  "Universidad Autónoma de Yucatán. Campus of Exact Sciences and Engineering.";
+
+const detailOrQuote =
+  "Expected graduation: July 2024. GPA: 4.0 (98/100).";
+
 
 const Education = () => {
   return (
-    <section id="education">
-      <div className="container">
-        <h2 className="section__title">Education</h2>
-        <p className="section__subtitle">
-          A brief overview of my educational background.
-        </p>
-
-        <div className="education__container container grid">
-          <div className="education__content grid">
-            <div className="education__data">
-              <h3 className="education__title">Bachelor's Degree</h3>
-              <span className="education__subtitle">
-                University of Puerto Rico
-              </span>
-              <span className="education__subtitle">
-                Computer Science
-              </span>
-              <div>
-                <span className="education__rounder"></span>
-                <span className="education__line"></span>
-              </div>
-              <p className="education__description">
-                I studied at the University of Puerto Rico, Mayaguez Campus, in
-                the Computer Science department. I graduated in May 2019.
-              </p>
-            </div>
-            <div className="education__data">
-              <h3 className="education__title">Master's Degree</h3>
-              <span className="education__subtitle">
-                University of Puerto Rico
-              </span>
-              <span className="education__subtitle">
-                Computer Engineering
-              </span>
-              <div>
-                <span className="education__rounder"></span>
-                <span className="education__line"></span>
-              </div>
-              <p className="education__description">
-                I studied at the University of Puerto Rico, Mayaguez Campus, in
-                the Computer Engineering department. I graduated in December
-                2020.
-              </p>
-            </div>
-          </div>
-
-          <div className="education__img grid">
-            <div className="education__img-overlay">
-              <img src={image} alt={imageAltText} className="education__img" />
-            </div>
-          </div>
-        </div>
+    <section className="padding" id="Education">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>Education</h2>
+        <p className="large">{description}</p>
+        <hr />
+        <p className="medium">{universityname}</p>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
       </div>
     </section>
   );
 };
 
 export default Education;
-
